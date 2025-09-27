c32653eb91206534cedaef404a0c640834c51add
Your Name <you@example.com>
2025-09-27 18:01:38 +0300

    HW1

---
+++ .history/src/components/MyExpenses_20250927163904.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="expens__conteiner">
+    <h1 class="text-ttl">Мои расходы</h1>
+    <div class="tbl">
+      <div class="tbl__expens">
+        <TblExpenses />
+      </div>
+      <div class="tbl__new-expens">
+        <NewExpenses />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import NewExpenses from './NewExpenses.vue'
+import TblExpenses from './TblExpenses.vue'
+</script>
+
+<style lang="scss">
+table {
+  border-spacing: 0px;
+}
+.expens__conteiner {
+  margin-left: 120px;
+  padding-top: 36px;
+  margin-right: 120px;
+  margin-bottom: 80px;
+  width: 1200px;
+}
+.text-ttl {
+  color: rgba(0, 0, 0, 1);
+  font-size: 32px;
+  font-weight: 700;
+}
+
+.tbl {
+  margin-top: 32px;
+  display: flex;
+  gap: 34px;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: stretch;
+}
+.tbl__expens {
+  width: 789px;
+  height: 618px;
+  border-radius: 30px;
+  box-shadow: 0px 20px 67px -12px rgba(0, 0, 0, 0.13);
+  background: rgba(255, 255, 255, 1);
+}
+.tbl__new-expens {
+  width: 379px;
+  height: 100%;
+  border-radius: 30px;
+  box-shadow: 0px 20px 67px -12px rgba(0, 0, 0, 0.13);
+  background: rgba(255, 255, 255, 1);
+}
+</style>
